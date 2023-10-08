ecd2aa1f6341212a38231fc4a2dbe811036bce0d
felipe konig <lipekonig@gmail.com>
2023-10-08 17:27:40 -0300

    configured changesets

--- packages/docs/src/stories/TexArea.stories.tsx
+++ packages/docs/src/stories/TexArea.stories.tsx
@@ -1,5 +1,5 @@
 import type { StoryObj, Meta } from '@storybook/react'
-import { Box, Text, TextArea, TextAreaProps } from '@ignite-ui/react'
+import { Box, Text, TextArea, TextAreaProps } from '@ignite-ui-felipe/react'
 
 export default {
   title: 'Form/TextArea Input',
---
+++ packages/eslint-config/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @ignite-ui-felipe/eslint-config
+
+## 2.0.0
+
+### Major Changes
+
+- Name in packages changed
